70ba9e4212ebb134179e98566e7d144605945be8
BasovichM <vbifakf@gmail.com>
2024-03-13 13:21:37 +0100

    Homework Session 6

--- src/test/java/org/prog/pages/AlloUaPage.java
+++ src/test/java/org/prog/pages/AlloUaPage.java
@@ -4,34 +4,23 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import java.time.Duration;
 import java.util.List;
 
+
 public class AlloUaPage {
-
     private final WebDriver driver;
-
-    private final static String SEARCH_PAGE_NUMBERS =
-            "//li[@class='pagination__item']/a[contains(text(), '%s')]";
-
-    private final static String PAGE_PREV_XPATH = "//*[@aria-label='prevision page']";
-    private final static String PAGE_NEXT_XPATH = "//*[@class='pagination__next']/a";
-
     public AlloUaPage(WebDriver driver) {
         this.driver = driver;
     }
-
     public void loadPage() {
         driver.get("https://allo.ua/");
     }
-
     public void searchForGoods(String searchValue) {
         WebElement searchInput = driver.findElement(By.name("search"));
         searchInput.sendKeys(searchValue);
         searchInput.sendKeys(Keys.ENTER);
     }
-
     public List<WebElement> getSearchResults() {
         return new WebDriverWait(driver, Duration.ofSeconds(30L))
                 .until(
@@ -39,7 +28,6 @@ public class AlloUaPage {
                                 1)
                 );
     }
-
     public boolean searchResultsContain(String value) {
         List<WebElement> searchResults = getSearchResults();
         Assert.assertFalse(searchResults.isEmpty(), "Search results must not be empty!");
@@ -52,51 +40,27 @@ public class AlloUaPage {
         }
         return valueIsPresent;
     }
-
-    public WebElement pagination() {
+    public WebElement pagination(){
         return driver.findElement(By.className("pagination"));
     }
-
     public void scrollToElement(WebElement e) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", e);
     }
-
-    public void switchToSearchPageByNumber(String number) {
-        new WebDriverWait(driver, Duration.ofSeconds(20L))
-                .until(ExpectedConditions.presenceOfElementLocated(
-                        By.xpath(String.format(SEARCH_PAGE_NUMBERS, number))))
-                .click();
-        waitForLoadingToFinish();
+    public void nextPage(){
+        WebElement paginationNext = driver.findElement(By.className("pagination__next"));
+        paginationNext.click();
     }
 
-    public void switchToPrevPage() {
-        switchPrevNextPage(PAGE_PREV_XPATH);
+    public void previousPage(){
+        WebElement paginationPrevious = driver.findElement(By.className("pagination__prev"));
+        paginationPrevious.click();
     }
 
-    public void switchToNextPage() {
-        switchPrevNextPage(PAGE_NEXT_XPATH);
+    public void pageByNumber(int number){
+        String xPathPlusNumber = "//*[@id='__layout']/div/div[1]/div[2]/div/div[2]/div[3]/ul/li[" + number + "]";
+        WebElement paginationByNumber = driver.findElement(By.xpath(xPathPlusNumber));
+        paginationByNumber.click();
+
     }
 
-    private void switchPrevNextPage(String xpath) {
-        scrollToElement(pagination());
-        if (isPaginationMissing(xpath)) {
-            System.out.println("Pagination is missing!");
-        } else {
-            new WebDriverWait(driver, Duration.ofSeconds(20L))
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)))
-                    .click();
-        }
-        waitForLoadingToFinish();
-    }
-
-    private boolean isPaginationMissing(String xpath) {
-        List<WebElement> pagination = new WebDriverWait(driver, Duration.ofSeconds(20L))
-                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpath)));
-        return pagination.isEmpty();
-    }
-
-    private void waitForLoadingToFinish() {
-        new WebDriverWait(driver, Duration.ofSeconds(20L))
-                .until(ExpectedConditions.invisibilityOfElementLocated(By.className("loading-block-gif")));
-    }
 }
